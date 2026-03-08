ff12b41f4720e3436ead6038a301f2675d6d3ab1
marlon <marlon.monterrosa.m@gmail.com>
2026-03-07 20:43:25 -0500

    creamos todas las clases y vamos hacer la conexion con la base de datos en mysql

---
+++ src/main/java/edu/iudigital/ConexionBD.java
@@ -0,0 +1,13 @@
+package edu.iudigital;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/autos_colombia";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+}
+
---
+++ src/main/java/edu/iudigital/VehiculoDAO.java
@@ -0,0 +1,4 @@
+package edu.iudigital;
+
+public class VehiculoDAO {
+}
