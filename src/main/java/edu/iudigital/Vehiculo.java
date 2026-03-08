10eb685dc1cedf5ed39f079a77ab15752d7735b3
YimyAsprilla <yimi.mosquera@est.iudigital.edu.co>
2026-03-07 20:59:00 -0500

    Se crea la clase vehiculo con sus atributos, contructor y getter

--- src/main/java/edu/iudigital/Vehiculo.java
+++ src/main/java/edu/iudigital/Vehiculo.java
@@ -1,4 +1,36 @@
 package edu.iudigital;
 
 public class Vehiculo {
+
+    private String placa;
+    private String tipo;
+    private String marca;
+    private String color;
+
+    // Se crea la clase vehiculo con sus atributos
+
+    public Vehiculo(String placa, String tipo, String marca, String color) {
+        this.placa = placa;
+        this.tipo = tipo;
+        this.marca = marca;
+        this.color = color;
+    }
+
+    // Se crean los getter
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getColor() {
+        return color;
+    }
 }
